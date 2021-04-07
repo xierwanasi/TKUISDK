a26b0ad94167638285171aa1acbf55baf16ad6ef
wangpeng <wangpengqqd@163.com>
2021-04-07 18:03:51 +0800

    remove 文件

--- Example/Pods/Target Support Files/SakuraKit/SakuraKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SakuraKit : NSObject
-@end
-@implementation PodsDummy_SakuraKit
-@end
--- Example/Pods/Target Support Files/TKUISDK/TKUISDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TKUISDK : NSObject
-@end
-@implementation PodsDummy_TKUISDK
-@end
